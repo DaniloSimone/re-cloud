014bfb8ff12fd1ed32ca4fc7455a94e7d33704d1
DaniloSimone <135861539+DaniloSimone@users.noreply.github.com>
2024-09-24 14:48:06 -0300

    si

--- back/schemas/usuarios.js
+++ back/schemas/usuarios.js
@@ -4,10 +4,6 @@ const usuarios = new Schema({
     nombre: {type:String, required:true},
     mail: {type:String, require:true, unique:true, index:true},
     contrasena: {type:String, require:true,index:true}
-
-
-
-
 });
 const usuario = model('Usuario', usuarios);
 export default usuario;  
